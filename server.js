d776ce94a6b271636ff566c609e923111835dfeb
Rayane83 <63799695+Rayane83@users.noreply.github.com>
2025-09-24 10:58:24 +0200

    Correction des fichiers manquants

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(session({
 
 // Configuration EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'templates'));
 
 // Mapping des rôles Discord
 const ROLE_MAPPING = {
