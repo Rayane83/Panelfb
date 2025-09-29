333dbccfa571ec05d0e7e02cc98587f13fec9be1
Rayane83 <63799695+Rayane83@users.noreply.github.com>
2025-09-29 18:49:21 +0200

    Corriger l'erreur de type MIME

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     https: false,
     host: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   define: {
     // Définir les variables d'environnement pour le build
